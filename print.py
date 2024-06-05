dd27ae8f3ac7eb584b3515b97c512a5f184f7390
pilarbachiller <pilar.bachiller@gmail.com>
2024-06-05 13:55:23 +0200

    new database

--- print.py
+++ print.py
@@ -1,27 +1,32 @@
+import sys
 import pickle
 
-with open('bot.db', 'rb') as fd:
+if len(sys.argv)!=2:
+    print('Please, specify a .db file')
+    exit()
+
+with open(sys.argv[1], 'rb') as fd:
     data = pickle.load(fd)
 
     #for k,v in data.items():
     #    print (k, v)
     #print()
 
-#    for k, v in data['users'].items():
- #       print('\n')
-  #      print(str(k)+' ')#+data['users'][k].uname)
-   #     for s, v in data['users'][k].input.items():
-    #        print(s, end='(')
-     #       c = '.'
-      #      if s in data['files']['main']:
-#                c = '+'
- #           if s in data['files']['basic']:
-  #              c = '*'
-   #         print(c, end=') ')
-    #        print(v, end=' ')
+    for k, v in data['users'].items():
+        print('\n')
+        print(str(k)+' ')#+data['users'][k].uname)
+        for s, v in data['users'][k].input.items():
+            print(s, end='(')
+            c = '.'
+            if s in data['files']['main']:
+                c = '+'
+            if s in data['files']['basic']:
+                c = '*'
+            print(c, end=') ')
+            print(v, end=' ')
 
 
-#    print('\n\n')
+    print('\n\n')
     for k, v in data['users'].items():
         print(k, data['users'][k].uname, len(v.input))
 
--- raters_analysis.py
+++ raters_analysis.py
@@ -29,23 +29,23 @@ for user1 in users:
 				if l2 in l_user1:
 					scores1_Q1.append(labels['users'][user1].input[l][0])
 					scores2_Q1.append(labels['users'][user1].input[l2][0])
-					scores1_Q2.append(labels['users'][user1].input[l][1])
-					scores2_Q2.append(labels['users'][user1].input[l2][1])
-					scores1_Q3.append(labels['users'][user1].input[l][2])
-					scores2_Q3.append(labels['users'][user1].input[l2][2])
+					# scores1_Q2.append(labels['users'][user1].input[l][1])
+					# scores2_Q2.append(labels['users'][user1].input[l2][1])
+					# scores1_Q3.append(labels['users'][user1].input[l][2])
+					# scores2_Q3.append(labels['users'][user1].input[l2][2])
 			else:
 				if l in l_user2:
 					scores1_Q1.append(labels['users'][user1].input[l][0])
 					scores2_Q1.append(labels['users'][user2].input[l][0])
-					scores1_Q2.append(labels['users'][user1].input[l][1])
-					scores2_Q2.append(labels['users'][user2].input[l][1])
-					scores1_Q3.append(labels['users'][user1].input[l][2])
-					scores2_Q3.append(labels['users'][user2].input[l][2])
+					# scores1_Q2.append(labels['users'][user1].input[l][1])
+					# scores2_Q2.append(labels['users'][user2].input[l][1])
+					# scores1_Q3.append(labels['users'][user1].input[l][2])
+					# scores2_Q3.append(labels['users'][user2].input[l][2])
 
 
-		if len(scores1_Q1)>100:
+		if len(scores1_Q1)>0: #100:
 			kcoeff1 = sk.cohen_kappa_score(scores1_Q1, scores2_Q1, labels=list(range(101)), weights='linear')
-			kcoeff2 = sk.cohen_kappa_score(scores1_Q2, scores2_Q2, labels=list(range(101)), weights='linear')
-			kcoeff3 = sk.cohen_kappa_score(scores1_Q3, scores2_Q3, labels=list(range(101)), weights='linear')			
+			kcoeff2 = 0#sk.cohen_kappa_score(scores1_Q2, scores2_Q2, labels=list(range(101)), weights='linear')
+			kcoeff3 = 0#sk.cohen_kappa_score(scores1_Q3, scores2_Q3, labels=list(range(101)), weights='linear')			
 			print(user1, "-", user2, "Q1:", kcoeff1, "Q2:", kcoeff2, "Q3:", kcoeff3, len(scores1_Q1))
 
